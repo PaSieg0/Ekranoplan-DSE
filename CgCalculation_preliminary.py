9a35653762fe1eb0a6e6adab3c3808949a9bf113
Qihao Guo <qihaoguo@tudelft.nl>
2025-05-07 09:56:51 +0200

    modified class Cg

--- CgCalculation_preliminary.py
+++ CgCalculation_preliminary.py
@@ -1,8 +1,29 @@
 import numpy as np
+class Cg:
+    def __init__(self, aircraft_type: int):
+        self.aircraft_type = aircraft_type
+        self.equation_map = {
+            1: self.Concept_2,      # Ekrano
+            2: self.Concept_7,      # Double fuselage
+            3: self.Concept_10,     # Sea plane
+            4: self.Concept_11,     # Double wing
+            5: self.Concept_13      # Flying airfoil
+        }
 
-class cg_estimation:
-    def __init__(self, Type):
-        self.type = type
+    def Cg_calculation(self):
+        match self.aircraft_type:
+            case 1:
+                pass  # To-do: Implement Concept_2 Cg calculation
+            case 2:
+                pass  # To-do: Implement Concept_7 Cg calculation
+            case 3:
+                pass  # To-do: Implement Concept_10 Cg calculation
+            case 4:
+                pass  # To-do: Implement Concept_11 Cg calculation
+            case 5:
+                pass  # To-do: Implement Concept_13 Cg calculation
+            case _:
+                raise ValueError("Invalid aircraft type")
 
-    def two(self):
-        if self.type = 2:
+
+
