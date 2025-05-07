76237d940a62401d8a765eef9c9df935b92110d4
mpjetri <m.pjetri@student.tudelft.nl>
2025-05-07 09:59:04 +0200

    pp

--- CgCalculation_preliminary.py
+++ CgCalculation_preliminary.py
@@ -3,24 +3,24 @@ class Cg:
     def __init__(self, aircraft_type: int):
         self.aircraft_type = aircraft_type
         self.equation_map = {
-            1: self.Concept_2,      # Ekrano
-            2: self.Concept_7,      # Double fuselage
-            3: self.Concept_10,     # Sea plane
-            4: self.Concept_11,     # Double wing
-            5: self.Concept_13      # Flying airfoil
+            2: self.Concept_2,      # Ekrano
+            7: self.Concept_7,      # Double fuselage
+            10: self.Concept_10,     # Sea plane
+            11: self.Concept_11,     # Double wing
+            13: self.Concept_13      # Flying airfoil
         }
 
     def Cg_calculation(self):
         match self.aircraft_type:
-            case 1:
-                pass  # To-do: Implement Concept_2 Cg calculation
             case 2:
+                pass  # To-do: Implement Concept_2 Cg calculation
+            case 7:
                 pass  # To-do: Implement Concept_7 Cg calculation
-            case 3:
+            case 10:
                 pass  # To-do: Implement Concept_10 Cg calculation
-            case 4:
+            case 11:
                 pass  # To-do: Implement Concept_11 Cg calculation
-            case 5:
+            case 13:
                 pass  # To-do: Implement Concept_13 Cg calculation
             case _:
                 raise ValueError("Invalid aircraft type")
