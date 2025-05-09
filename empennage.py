70a07026fec2d1e1fa6b329730e3c9a0690675d4
adam91101 <elassaladam@gmail.com>
2025-05-09 17:04:25 +0200

    Added tail volumes to json files. Changed empennage file to get all values from json file except for Xcg.

--- empennage.py
+++ empennage.py
@@ -3,44 +3,35 @@ import pandas as pd
 import numpy as np
 from utils import Data
 
-def hor_tail_area(V_h, S, MAC, X_h, Xcg_aft):
+def hor_tail_area(Xcg_aft):
     """
     Calculate the horizontal tail area based on the aircraft data.
     
     Returns:
         float: The horizontal tail area.
     """
-    S_h = V_h * S * MAC / (X_h - (data.data['X_LEMAC'] + Xcg_aft*data.data['MAC']))
+    S_h = data.data['inputs']['V_h'] * data.data['outputs']['max']['S'] * data.data['outputs']['max']['MAC'] / (data.data['outputs']['general']['l_fuselage'] - (data.data['outputs']['wing_design']['X_LEMAC'] + Xcg_aft*data.data['outputs']['max']['MAC']))
 
     return S_h
 
-def ver_tail_area(V_v, S, b, X_v, Xcg_aft):
+def ver_tail_area(Xcg_aft):
     """
     Calculate the vertical tail area based on the aircraft data.
     
     Returns:
         float: The vertical tail area.
     """
-    S_v = V_v * S * b / (X_v - (data.data['X_LEMAC'] + Xcg_aft*data.data['MAC']))
+    S_v = data.data['inputs']['V_v'] * data.data['outputs']['max']['S'] * data.data['outputs']['max']['b'] / (data.data['outputs']['general']['l_fuselage'] - (data.data['outputs']['wing_design']['X_LEMAC'] + Xcg_aft*data.data['outputs']['max']['MAC']))
 
     return S_v
 
 if __name__ == "__main__":
     data = Data("design1.json")
 
-    V_h = 3.662649875 # From statistics, taken from Aircraft Data Excel
-    V_v = 0.273372017 # From statistics, taken from Aircraft Data Excel
-
-    S = data.data['design']['S']
-    MAC = data.data['design']['MAC']
-    b = data.data['design']['b']
-    
-    X_h = 75 #m From Excel
-    X_v = 75 #m From Excel
     Xcg_aft = 0.30440835377790804 # From CG calculation
 
-    S_h = hor_tail_area(V_h, S, MAC, X_h, Xcg_aft)
-    S_v = ver_tail_area(V_v, S, b, X_v, Xcg_aft)
+    S_h = hor_tail_area( Xcg_aft)
+    S_v = ver_tail_area( Xcg_aft)
 
     print(f"Horizontal Tail Area: {S_h} m^2")
     print(f"Vertical Tail Area: {S_v} m^2")
