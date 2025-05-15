72871beb2af023f68224c86a2da960ef13a9a274
succhido <leonardo.sigolo@gmail.com>
2025-05-15 13:54:18 +0200

    Update engine_height.py

--- engine_height.py
+++ engine_height.py
@@ -16,21 +16,29 @@ class EngineHeight:
         self.dihedral = self.data.data['outputs']['wing_design']['dihedral']
         self.n_engines = self.data.data['inputs']['n_engines']
         self.MTOM = self.data.data['outputs']['design']['MTOM']
+        self.n_fuselage = self.data.data['inputs']['n_fuselages']
+        if 'fuselage_separation' in self.data.data['outputs']['general']:
+            self.fuselage_separation = self.data.data['outputs']['general']['fuselage_separation']
+        
 
     def calculate_floating_depth(self):    
-        return self.MTOM / (self.sea_water_density * self.l_fuselage * self.d_fuselage)
+        return self.MTOM / (self.sea_water_density * self.l_fuselage * self.d_fuselage * self.n_fuselage)
 
     def calculate_engine_positions(self):
-        floating_depth = self.calculate_floating_depth()
-        
-        # Calculate y positions
-        y_engines = np.zeros(int(self.n_engines/2))
-        for j in range(int(self.n_engines/2)):
-            if j == 0:
-                y_engines[j] = self.d_fuselage/2 + (self.diameter_engine/2)
-            else:
-                y_engines[j] = y_engines[j-1] + (self.diameter_engine + self.clearance_enginetips)
-
+        if self.n_fuselage == 1:
+            floating_depth = self.calculate_floating_depth()
+            # Calculate y positions
+            y_engines = np.zeros(int(self.n_engines/2))
+            for j in range(int(self.n_engines/2)):
+                if j == 0:
+                    y_engines[j] = self.d_fuselage/2 + (self.diameter_engine/2)
+                else:
+                    y_engines[j] = y_engines[j-1] + (self.diameter_engine + self.clearance_enginetips)
+        else:
+            floating_depth = self.calculate_floating_depth()
+            if self.fuselage_separation > self.diameter_engine*2+self.clearance_engine_fuselage*2 + self.clearance_enginetips:
+                y_engines = np.zeros(int(self.n_engines/2))
+                
         # Calculate z positions and clearances
         z_engines = np.zeros_like(y_engines)
         if self.wing_type == "HIGH":
@@ -38,7 +46,7 @@ class EngineHeight:
         elif self.wing_type == "LOW":
             z_engines = np.tan(np.deg2rad(self.dihedral))*y_engines
         
-        wing_tip_clearance = z_engines - (self.diameter_engine/2) - floating_depth
+        wing_tip_clearance = z_engines - floating_depth
         
         return y_engines, z_engines, wing_tip_clearance
 
@@ -49,8 +57,7 @@ def main():
         engine_height = EngineHeight(aircraft_data)
         y_engines, _, wing_tip_clearance = engine_height.calculate_engine_positions()
         
-        print(f"Clearance of design {i} engines: {wing_tip_clearance} from water in sea state 0.")
-        print(f"Engine placement in y of design {i}: {y_engines} from center of fuselage.")
+        print(f"Engine clearance distance for design {i} is {wing_tip_clearance} m")
 
 if __name__ == "__main__":
     main()
