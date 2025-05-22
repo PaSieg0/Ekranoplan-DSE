0793838f7eb664b97e31e77f4217e1b650f1c291
marnick1268 <M.A.Stoll@student.tudelft.nl>
2025-05-22 11:58:28 +0200

    push

--- MidtermReview/costs pres.py
+++ MidtermReview/costs pres.py
@@ -9,5 +9,7 @@ xlst=np.array(['C5','C17','C130'])
 
 fig=plt.figure()
 ax=fig.add_subplot()
+ax.set_ylabel('Costs ($/km/tonnes)')
+ax.set_title('Operational Costs')
 ax.bar(xlst,ylst,color=['green','purple','blue'])
 plt.show()
