a99d7669ef82c4da60a75b3f90d0fb04c7be2b98
Jaime de Salas <jaimedesalasjuanas@gmail.com>
2025-05-26 17:20:33 +0200

    si

--- Optimum_Performance/Optimum_climb.py
+++ Optimum_Performance/Optimum_climb.py
@@ -4,7 +4,7 @@ import os
 import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from utils import Data, ISA, MissionType
-from AltitudeVelocity import AltitudeVelocity
+from Class_I.AltitudeVelocity import AltitudeVelocity
 from tqdm import tqdm
 
 class OptimumClimb(AltitudeVelocity):
