a7747f9b573677f7ab137860f12365e0a75230da
Qihao Guo <qihaoguo@tudelft.nl>
2025-05-26 15:53:59 +0200

    updated take off mechanics

--- Class_II/Take_off_mechanics.py
+++ Class_II/Take_off_mechanics.py
@@ -12,30 +12,18 @@ class EmpType(Enum):
     H_TAIL = auto()
     NONE = auto()
 
-class EnvironmentData:
-    def __init__(self):
-        self.rho_air = 1.225       # kg/m³ at sea level, 15°C
-        self.P0 = 101325     # Pa
-        self.Temp_air = 288.15  # K (15°C)
-        self.rho_water = 1025   # kg/m³ at sea level
-        self.g = 9.80665
 
-class Angles:
-    def deg2rad(self, deg):
-        return deg * np.pi / 180
 
-    def __init__(self):
-        self.AOA = self.deg2rad(5.0)
-        self.ThrustVectorAngle = self.deg2rad(3.0) #im just capping everything here
-        self.HydroLiftAngle = self.deg2rad(0.0)
-        self.HydroDragAngle = self.deg2rad(0.0)
+class Take_off_mechanics:
 
-class Take_off_dynamic_model:
-    def __init__(self, aircraft_data: Data, env: EnvironmentData, angles: Angles):
+
+    def __init__(self, aircraft_data: Data):
         self.design_number = aircraft_data.data['design_id']
         self.design_file = f"design{self.design_number}.json"
         self.aircraft_data = aircraft_data
         self.tail_type = EmpType[aircraft_data.data['inputs']['tail_type']]
+        if self.tail_type == EmpType.NONE:
+            return
 
         self.S = self.aircraft_data.data['outputs']['wing_design']['S']
         self.hull_surface = self.aircraft_data.data['outputs']['general']['hull_surface']
@@ -50,52 +38,129 @@ class Take_off_dynamic_model:
 
         self.thrust = 110000*self.aircraft_data.data['outputs']['inputs']['n_engines']
 
-        self.env = env
-        self.angles = angles
+        #hardcoded variables for now, change these later on to be self-iterating
+        self.rho_air = 1.225  # kg/m³ at sea level, 15°C
+        self.P0 = 101325  # Pa
+        self.Temp_air = 288.15  # K (15°C)
+        self.rho_water = 1025  # kg/m³ at sea level
+        self.g = 9.80665
+        self.AOA = 5.0
+        self.ThrustVectorAngle = 3.0  # im just capping everything here
+        self.HydroLiftAngle = 0.0
+        self.HydroDragAngle = 0.0
+
+        #hull shaped fuselage initial sizing
+        self.triangle_height = 3
+        self.width = 6
+        self.length = 60
 
 
-        def get_fluid_force(coeff, rho, v, S, theta):
-            F_mag = 0.5 * coeff * rho * v ** 2 * S
-            Fx = F_mag * np.cos(theta)
-            Fy = F_mag * np.sin(theta)
-            return np.array([Fx, Fy])
+    def immersed_volume(self, h):
+        """Piecewise volume as function of immersion depth h (m)"""
+        if h <= self.triangle_height:
+            return 0.5 * self.width * h * self.length
+        else:
+            triangle_vol = 0.5 * self.width * self.triangle_height * self.length
+            rect_vol = self.width * (h - self.triangle_height) * self.length
+            return triangle_vol + rect_vol
 
-        def get_bouyancy_force():
-            Fx = 0
-            Fy =
-            return np.array([Fx, Fy])
+    def get_initial_submerged_volume(self):
+        """Return submerged volume required to balance MTOW"""
+        # Solve V such that buoyant force = weight
+        target_volume = self.MTOW / (self.rho_water * self.g)
 
-        def simulate_takeoff(self):
-            dt = 0.1
-            t_max = 600
+        # Use bisection method to find h
+        h_low = 0
+        h_high = 5
+        for _ in range(100):
+            h_mid = 0.5 * (h_low + h_high)
+            vol = self.immersed_volume(h_mid)
+            if vol > target_volume:
+                h_high = h_mid
+            else:
+                h_low = h_mid
+        return self.immersed_volume(h_mid), h_mid
 
-            time = [0]
-            velocity = [0]
-            distance = [0]
+    def update_bouyancy_force(self, h):
+        """Returns buoyancy force for a given immersion depth h (m)"""
+        V = self.immersed_volume(h)
+        Fx = 0
+        Fy = self.rho_water * self.g * V
+        return np.array([Fx, Fy])
 
-            v = 0
-            x = 0
-            t = 0
-            g = self.env.g
-            m = self.MTOW/g
-            L = get_fluid_force(self.CLmax_takeoff, self.env.rho, v, self.S,angles.AOA, 1)
-            D = get_fluid_force(self.Cd, self.env.rho, v, self.S, angles.AOA, 0)
-            L_hydro = get_fluid_force(self.Cl_hydro, self.env.rho_water, v, self.hull_surface, angles.HydroLiftAngle, 1)
-            D_hydro = get_fluid_force(self.Cd_water, self.env.rho_water, v, self.hull_surface, angles.HydroDragAngle,0)
-            F_bouy = get_bouyancy_force()
+    def get_new_submerged_volume(self, h):
+        """Returns volume submerged at depth h"""
+        return self.immersed_volume(h)
 
-            while t < t_max:
-                F = L-D+L_hydro-D_hydro+F_bouy
-
-                a = F/m
-
-                v += a * dt
-                x += v * dt
-                t += dt
-
-                time.append(t)
-                velocity.append(v)
-                distance.append(x)
+    def get_fluid_force(coeff, rho, v, S, theta):
+        F_mag = 0.5 * coeff * rho * v ** 2 * S
+        Fx = F_mag * np.cos(theta)
+        Fy = F_mag * np.sin(theta)
+        return np.array([Fx, Fy])
 
 
-            return time, velocity, distance
+    def simulate_takeoff(self):
+        dt = 0.1
+        t_max = 600
+
+        time = [0]
+        velocity = [0]
+        distance = [0]
+
+        v = 0
+        x = 0
+        t = 0
+        g = self.g
+        m = self.MTOW/g
+        L = self.get_fluid_force(self.CLmax_takeoff, self.rho_air, v, self.S, np.deg2rad(self.AOA))
+        D = self.get_fluid_force(self.Cd, self.rho_air, v, self.S, np.deg2rad(self.AOA))
+        L_hydro = self.get_fluid_force(self.CL_hydro, self.rho_water, v, self.hull_surface, np.deg2rad(self.HydroLiftAngle))
+        D_hydro = self.get_fluid_force(self.Cd_water, self.rho_water, v, self.hull_surface, np.deg2rad(self.HydroDragAngle))
+        F_bouy = self.update_bouyancy_force(x)
+
+        while t < t_max:
+            F = L-D+L_hydro-D_hydro+F_bouy
+
+            a = F/m
+
+            v += a * dt
+            x += v * dt
+            t += dt
+
+            time.append(t)
+            velocity.append(v)
+            distance.append(x)
+
+
+        return time, velocity, distance
+
+    def run_simulation(self, plot=True):
+        time, velocity, distance = self.simulate_takeoff()
+
+        if plot:
+            import matplotlib.pyplot as plt
+            plt.figure(figsize=(12, 6))
+
+            plt.subplot(2, 1, 1)
+            plt.plot(time, velocity, label="Velocity (m/s)")
+            plt.ylabel("Velocity (m/s)")
+            plt.grid(True)
+            plt.legend()
+
+            plt.subplot(2, 1, 2)
+            plt.plot(time, distance, label="Distance (m)", color="orange")
+            plt.xlabel("Time (s)")
+            plt.ylabel("Distance (m)")
+            plt.grid(True)
+            plt.legend()
+
+            plt.tight_layout()
+            plt.show()
+
+        return time, velocity, distance
+
+
+if __name__ == "__main__":
+    data = Data("design3.json")
+    model = Take_off_mechanics(data)
+    time, velocity, distance = model.run_simulation()
