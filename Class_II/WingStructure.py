Fix calculation of top bending stress in StressAnalysisWing class

Class_II/WingStructure.py
@@ -296,9 +296,9 @@ class WingStructure:
     def beam_standard_Iyy(self, length, thickness, distance, angle):
         A_web = length * thickness
         I_web = (thickness*length**3*np.cos(angle)**2) / 12
-        I_xx = I_web + A_web * distance ** 2
+        I_yy = I_web + A_web * distance ** 2
 
-        return I_xx
+        return I_yy
     
     def beam_standard_Ixy(self, length, thickness, xy, angle):
 
@@ -306,9 +306,9 @@ class WingStructure:
 
         I_web = (thickness*length**3*np.sin(angle)*np.cos(angle)) / 12
 
-        I_xx = I_web + A_web * xy
+        I_xy = I_web + A_web * xy
 
-        return I_xx
+        return I_xy
 
     def get_polar_moment(self):
 