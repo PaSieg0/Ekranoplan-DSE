Fix downwash gradient calculation in Cm_alphadot method

Aero_stab/Derivatives_datcom_asym.py
@@ -178,8 +178,8 @@ class DerivativesDatcom_asym:
         float: Roll moment coefficient.
         """
         # p2580
-        B = np.sqrt(1 - 0.34**2 * np.cos(np.radians(self.Delta_c4))**2) # p.2522
-        Clr__cl_M0 = .24 #p.2589
+        B = np.sqrt(1 - self.M**2 * np.cos(np.radians(self.Delta_c4))**2) # p.2522
+        Clr__cl_M0 = 0.24 #p.2589
         Clr__Cl = (1 + (self.A*(1-B**2))/(2*B*(self.A*B + 2*np.cos(np.radians(self.Delta_c4)))) + (self.A*B+2*np.cos(np.radians(self.Delta_c4)))/(self.A*B+4*np.cos(np.radians(self.Delta_c4))) * np.tan(np.radians(self.Delta_c4))**2 / 8 * Clr__cl_M0) / (1 + (self.A + 2 * np.cos(np.radians(self.Delta_c4))) / (self.A + 4 * np.cos(np.radians(self.Delta_c4))) * np.tan(np.radians(self.Delta_c4))**2 / 8) #p.2581
 
         clB__cl_s = -0.002/5 # from plot->p1563

Aero_stab/Derivatives_datcom_sym.py
@@ -113,7 +113,7 @@ class DerivativesDatcom_sym:
         Cm_alphadot_wb = (K_bw + K_wb) * (self.Sh/self.S)*(self.c_h/self.MAC)**2 * Cm_alphadot_e + Cm_alphadot_B * ((S_b / self.S)*(self.l_b/self.MAC)**2)
         
         l_h = self.x_h - self.x_cg
-        downwash_gradient = (2 * self.Cl_alpha / (np.pi * self.A))*(180/np.pi)
+        downwash_gradient = (2 * self.Cl_alpha / (np.pi * self.A))
         Cm_alphadot_tail = -(self.Cl_alpha * 1 * downwash_gradient * self.Sh*l_h**2) / (self.S * self.MAC**2)
         return Cm_alphadot_wb - Cm_alphadot_tail
     