90863d542e48805bc0dfb98c1c2ea59410394676
shuardn <118279282+shuardn@users.noreply.github.com>
2025-06-06 13:34:11 +0200

    Update Moment_of_inertia.py

--- Data/Moment_of_inertia.py
+++ Data/Moment_of_inertia.py
@@ -1,7 +1,7 @@
 import os
 import sys
 import numpy as np
-project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '../..'))
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(project_root)
 from utils import Data  
 
