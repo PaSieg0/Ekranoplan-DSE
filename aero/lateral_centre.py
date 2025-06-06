2bb8a071d32d9f36ebc4f7e755a5cfe22127c0cd
marnick1268 <M.A.Stoll@student.tudelft.nl>
2025-06-06 14:38:29 +0200

    intergrat

--- aero/lateral_centre.py
+++ aero/lateral_centre.py
@@ -5,7 +5,7 @@ import os
 import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from utils import Data, MissionType, ISA, AircraftType
-
+import scipy.integrate as sc
 
 class lateral_centre():
     def __init__(self):
@@ -28,8 +28,8 @@ class lateral_centre():
         #print(self.cl_pos)
     
     def intergrate(self):
-        I_2=np.trapezoid(self.cl,self.pos)
-        I_1=np.trapezoid(self.cl_pos,self.pos)
+        I_2=sc.trapezoid(self.cl,self.pos)
+        I_1=sc.trapezoid(self.cl_pos,self.pos)
         # print(I_2)
         
         I=I_1/I_2
