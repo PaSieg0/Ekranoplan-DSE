cabaeeac6453df7179b87ec3b59aab6cf0bda6a8
shuardn <118279282+shuardn@users.noreply.github.com>
2025-06-06 13:27:55 +0200

    Update Moment_of_inertia.py
    This needs to be fixed

--- Data/Moment_of_inertia.py
+++ Data/Moment_of_inertia.py
@@ -1,8 +1,10 @@
 import os
 import sys
 import numpy as np
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from utils import *
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '../..'))
+sys.path.append(project_root)
+from utils import Data  
+
 
 class MomentOfInertia:
     def __init__(self, aircraft_data: Data):
@@ -32,8 +34,29 @@ class MomentOfInertia:
         self.engine2_d = np.sqrt(self.engine2_y**2 + self.engine2_z**2)
         self.engine3_d = np.sqrt(self.engine3_y**2 + self.engine3_z**2)
         self.wing_d = self.h_b_wing * self.b_wing
+
+    def calculate_Ixx(self):
+        self.calculate_radii()
+        self.calculate_distances()
+        self.Ixx_fuselage = 0.5 * (self.fuselage_weight+self.cargo_weight) * (self.r_fuselage**2)
+        self.Ixx_wing = self.wing_weight * self.wing_d**2
+        self.Ixx_engine1 = (0.5 * self.engine_weight * self.r_engine**2) + (self.engine_weight * self.engine1_d**2)
+        self.Ixx_engine2 = (0.5 * self.engine_weight * self.r_engine**2) + (self.engine_weight * self.engine2_d**2)
+        self.Ixx_engine3 = (0.5 * self.engine_weight * self.r_engine**2) + (self.engine_weight * self.engine3_d**2)
+        self.Ixx_total = (self.Ixx_fuselage +
+                          self.Ixx_wing + 
+                          self.Ixx_engine1 + 
+                          self.Ixx_engine2 + 
+                          self.Ixx_engine3)
+        return (self.Ixx_total)
+
+if __name__ == "__main__":
+    aircraft_data = Data("design3.json")
+    print(f"Ixx:{self.calculate_Ixx()}")    
+
     
     
+
 
     
 
