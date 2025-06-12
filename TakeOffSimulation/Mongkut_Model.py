eef440d98697914a8121dd049262e15528eeba19
OM00n <owenmoon2310@gmail.com>
2025-06-12 08:40:47 +0200

    wtf is going on

--- TakeOffSimulation/Mongkut_Model.py
+++ TakeOffSimulation/Mongkut_Model.py
@@ -1,7 +1,7 @@
 import os
 import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from utils import Data
+from utils import Data#, W2hp, ms2kts, N2lbf
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -11,13 +11,71 @@ class Simulation:
         self.aircraft_data = aircraft_data
         self.design_file = f"design{self.aircraft_data.data['design_id']}.json"
 
-        self.t_end = 100
+        self.t_end = 190
         self.dt = 0.1
         self.t = 0.0
 
 
         self.C_delta_0 = self.calculate_C_delta_0()
-        self.h = -3.1
+        self.determine_thrust_function()
+
+
+        self.x = 0
+        self.y = -3.1
+        self.v_x = 0
+        self.v_y = 0
+        self.a_x = 0
+        self.a_y = 0
+
+        self.time_list = []
+
+        self.x_list = []
+        self.y_list = []
+        self.v_y_list = []
+        self.v_x_list = []
+        self.a_x_list = []
+        self.a_y_list = []
+
+
+        self.velocities = []
+        self.R_list = []
+        self.D_list = []
+        self.Thrust_list = []
+        self.R_froude_list = []
+        self.buoyance_list = []
+        self.total_drag_list = []
+        self.total_power_list = []
+        self.L_list = []
+        self.F_x_list = []
+        self.F_y_list = []
+
+    def determine_thrust_function(self):
+        vc = (self.aircraft_data.data['requirements']['cruise_speed'])
+        vh = (self.aircraft_data.data['outputs']['optimum_speeds']['max'])
+        T_static  = (110000)
+        T_C = (312000) #181000
+        T_H = (223000) #223000
+        eta_p = self.aircraft_data.data['inputs']['engine']['engine_efficiency']
+        P_BHP = self.aircraft_data.data['inputs']['engine']['engine_power']
+        A_matrix = np.array([
+            [0, 0, 0, 1],
+            [vc**3, vc**2, vc, 1],
+            [3*vc**2, 2*vc, 1, 0],
+            [vh**3, vh**2, vh, 1]
+        ])
+
+        B_matrix = np.array([
+            [T_static],
+            [T_C],
+            [-eta_p*P_BHP/(vc**2)],
+            [T_H]
+        ])
+
+        x = np.linalg.solve(A_matrix, B_matrix)
+        A, B, C, D = x.flatten()
+        # print(A, B, C, D)
+        self.Thrust_function = lambda x: A * x**3 + B * x**2 + C * x + D
+
 
     def calculate_C_delta_0(self):
         MTOW = self.aircraft_data.data['outputs']['max']['MTOW']
@@ -27,28 +85,62 @@ class Simulation:
         return (MTOW) / (rho_water * g * B**3)
     
     def calculate_wetted_width(self):
-        h = self.h
+        h = self.y
         if h <= -1.6:
             return self.aircraft_data.data['outputs']['fuselage_dimensions']['w_fuselage']
         elif -1.6 < h <+ 0:
             return -self.aircraft_data.data['outputs']['fuselage_dimensions']['w_fuselage'] * h / 1.6
         elif h >= 0:
-            print("Returning 0")
             return 0
         
+    def calculate_wetted_area(self):
+        hull_length = self.aircraft_data.data['outputs']['fuselage_dimensions']['l_nose'] + self.aircraft_data.data['outputs']['fuselage_dimensions']['l_forebody']
+        wetted_width = self.calculate_wetted_width()
+        if wetted_width < 0:
+            raise ValueError("Wetted width cannot be negative. Check the height (h) value.")
+        if wetted_width == 0:
+            return 0
+        h = self.y
+        wetted_hypotenuse = 2*np.sqrt((wetted_width/2)**2 + h**2)
+        wetted_area = hull_length * wetted_hypotenuse
+        self.wetted_area = wetted_area
+        return wetted_area
     
+    def calculate_wetted_volume(self):
+        if self.y <= -1.6:
+            triangle_area = 0.5*1.6*self.aircraft_data.data['outputs']['fuselage_dimensions']['w_fuselage']
+            rect_area = (-self.y - 1.6) * self.aircraft_data.data['outputs']['fuselage_dimensions']['w_fuselage']
+        if -1.6 < self.y <= 0:
+            triangle_area = 0.5*-self.y*self.calculate_wetted_width()
+            rect_area = 0
+        if self.y > 0:
+            return 0
+        tot_area = triangle_area + rect_area
+        hull_length = self.aircraft_data.data['outputs']['fuselage_dimensions']['l_nose'] + self.aircraft_data.data['outputs']['fuselage_dimensions']['l_forebody']
+        wetted_volume = hull_length * tot_area
+        # print(wetted_volume)
+        return wetted_volume
+
+
+
     def update_C_delta(self):
         B = self.aircraft_data.data['outputs']['fuselage_dimensions']['w_fuselage'] #self.calculate_wetted_width()
         # print(f"Wetted width (B): {B}")
         if B < 0:
             raise ValueError("Wetted width cannot be negative. Check the height (h) value.")
         if B == 0:
-            print("Returning infinity for C_delta due to zero wetted width.")
+            # print("Returning infinity for C_delta due to zero wetted width.")
             return float('inf')
         self.C_delta = 1/(B**3)
 
+    def update_buoyancy(self):
+        rho_water = self.aircraft_data.data['rho_water']
+        g = 9.81
+        wetted_volume = self.calculate_wetted_volume()
+        self.buoyancy = rho_water * g * wetted_volume
+
     def update_C_v(self):
-        v = self.v
+        v = self.v_x
         g = 9.81
         B = self.aircraft_data.data['outputs']['fuselage_dimensions']['w_fuselage']
         self.C_v = (v) / (np.sqrt(g * B))
@@ -58,15 +150,39 @@ class Simulation:
         self.C_R = 0.05789 * np.exp(-((C_V - 1.907) / (0.7561))**2) + 0.0273 * np.exp(-((C_V - 1.347) / (0.2536))**2) - 0.3322 * np.exp(-((C_V - 23.41) / (11.74))**2) + 0.07924 * np.exp(-((C_V - 3.227) / (1.951))**2)
 
     def update_C_R_delta(self):
-        print(f"C_R: {self.C_R}, C_delta: {self.C_delta}, C_delta_0: {self.C_delta_0}")
         self.C_R_delta = self.C_R * (self.C_delta / self.C_delta_0)
 
     def update_R(self):
         rho_water = self.aircraft_data.data['rho_water']
         g = 9.81
-        self.R = self.C_R_delta * rho_water * g * self.aircraft_data.data['outputs']['fuselage_dimensions']['w_fuselage']**3
+        self.R = max(0, self.C_R_delta * rho_water * g * self.aircraft_data.data['outputs']['fuselage_dimensions']['w_fuselage']**3)
 
-        
+    def update_Thrust(self):
+        v = self.v_x
+        if v < 0:
+            raise ValueError("Velocity cannot be negative.")
+        self.Thrust = self.Thrust_function(v)
+
+    def update_R_froude(self):
+        f = 0.01
+        Sw = self.calculate_wetted_area()
+        n = 1.83
+        self.R_froude = f * Sw * self.v_x**n
+
+
+    def update_D(self):
+        Cd0 = self.aircraft_data.data['inputs']['Cd0']
+        A = self.aircraft_data.data['outputs']['wing_design']['aspect_ratio']
+        e = self.aircraft_data.data['inputs']['oswald_factor']
+        Cl = 2.15
+        Cd = Cd0 + (Cl**2 / (np.pi * A * e))
+        print(f"Cd: {Cd}, Cl: {Cl}, A: {A}, e: {e}")
+        self.D = 0.5 * 1.225 * (self.v_x**2) * self.aircraft_data.data['outputs']['wing_design']['S'] * Cd
+
+    def update_L(self):
+        Cl = 2.15
+        self.L = 0.5 * 1.225 * (self.v_x**2) * self.aircraft_data.data['outputs']['wing_design']['S'] * Cl
+
 
     def update_all_states(self):
         self.update_C_delta()
@@ -74,21 +190,97 @@ class Simulation:
         self.update_C_R()
         self.update_C_R_delta()
         self.update_R()
+        self.update_Thrust()
+        self.update_R_froude()
+        self.update_D()
+        self.update_L()
+        self.update_buoyancy()
         # print(f"Height: {self.h}, Velocity: {self.v}, C_R_delta: {self.C_R_delta}, R: {self.R}")
         # print(f"C_delta: {self.C_delta}, C_v: {self.C_v}, C_R: {self.C_R}")
 
+    def store_all_states(self):
+        self.time_list.append(self.t)
+        self.x_list.append(self.x)
+        self.y_list.append(self.y)
+        self.v_x_list.append(self.v_x)
+        self.v_y_list.append(self.v_y)
+        self.a_x_list.append(self.a_x)
+        self.a_y_list.append(self.a_y)
+        self.R_list.append(self.R)
+        self.D_list.append(self.D)
+        self.Thrust_list.append(self.Thrust*self.r)
+        self.R_froude_list.append(self.R_froude)
+        self.L_list.append(self.L)
+        self.buoyance_list.append(self.buoyancy)
+        self.total_drag = self.R + self.D + self.R_froude
+        self.total_drag_list.append(self.total_drag)
+        self.total_power = (self.Thrust * self.v_x)
+        self.total_power_list.append(self.total_power)
+
+    def plot_results(self):
+        plt.plot(self.time_list, self.R_list, 'k-', label='R')
+        # plt.plot(self.time_list, self.D_list, 'r-', label='D')
+        # plt.plot(self.time_list, self.Thrust_list, 'g-', label='Thrust')
+        # plt.plot(self.time_list, self.R_froude_list, 'm-', label='R_froude')
+        # plt.plot(self.time_list, self.total_drag_list, 'c-', label='Total D')
+        # plt.plot(self.time_list, self.y_list, "k-", label='y')
+        # plt.plot(self.time_list, self.buoyance_list)
+        # plt.plot(self.time_list, self.L_list, label='L')
+        # plt.plot(self.time_list, self.v_x_list, label='V_X')
+        # plt.plot(self.time_list, self.v_y_list, label='V_X')
+        # plt.plot(self.time_list, self.a_x_list, label='A_X')
+        plt.xlabel('Time (S)')
+        plt.ylabel('Forces (N)')
+        plt.title(f'Simulation at Height: {self.y:.2f} m')
+        handles, labels = plt.gca().get_legend_handles_labels()
+        unique = dict()
+        for h, l in zip(handles, labels):
+            if l not in unique:
+                unique[l] = h
+        plt.legend(unique.values(), unique.keys())
+        plt.show()
+
+        plt.plot(self.time_list, self.total_power_list)
+        plt.show()
+
         
     def run_simulation(self):
-        for v in np.arange(0, 70, 0.1):
-            self.h += 1
-            self.v = v
+        self.r = 0
+        self.update_all_states()
+        self.store_all_states()
+
+        # for v in range(0, 70):
+        #     self.v_x = v
+        #     self.update_Thrust()
+        #     plt.plot(self.v_x, self.Thrust, 'ro-')
+        # plt.show()
+            
+        while self.t < self.t_end:
+
+            self.r = 0.1 + 0.9*self.t/10 if self.t < 10 else 1
+
+            self.F_x = self.Thrust*self.r - self.R_froude - self.D - self.R
+            self.a_x = self.F_x / self.aircraft_data.data['outputs']['max']['MTOM']
+            self.F_y = self.buoyancy + self.L - self.aircraft_data.data['outputs']['max']['MTOW']
+            self.a_y = self.F_y / self.aircraft_data.data['outputs']['max']['MTOM']
+
+            self.v_x += self.a_x * self.dt
+            print(self.v_x)
+            self.v_y += self.a_y * self.dt
+
+            self.x += self.v_x * self.dt
+            self.y += self.v_y * self.dt
+
+            self.t += self.dt
+
             self.update_all_states()
+            self.store_all_states()
 
-            plt.plot(v, self.R, 'ro')
-
-        plt.show()
+        self.plot_results()
 
 if __name__ == "__main__":
     aircraft_data = Data('design3.json')
     simulation = Simulation(aircraft_data)
     simulation.run_simulation()
+
+    # print(simulation.Thrust_list[0])
