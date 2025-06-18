83655e863eafa740f0154fd8b5dbec9091966039
marnick1268 <M.A.Stoll@student.tudelft.nl>
2025-06-18 15:49:15 +0200

    goof luck owen

--- budgets/power_budget.py
+++ budgets/power_budget.py
@@ -1,9 +1,13 @@
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import pandas as pd
 
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import pandas as pd
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from utils import plt
 
 # Data from the first image
 data = {
